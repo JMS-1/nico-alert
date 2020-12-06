0815b8fcb8408589894c39472d39a3c194597ed8
Dr. Jochen Manns <manns@insynergie.de>
2020-12-06 14:40:27 +0100

    preparing hardware

--- code.ino
+++ code.ino
@@ -1,8 +1,10 @@
-#include <Adafruit_NeoPixel.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+
+#include <ESP8266WebServer.h>
+
+#include <Adafruit_NeoPixel.h>
 
 #include "private.h"
 
@@ -10,63 +12,105 @@ const char *ssid = STASSID;
 const char *password = STAPSK;
 
 ESP8266WebServer server(80);
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(7, D2, NEO_GRB + NEO_KHZ800);
 
-const int led = 13;
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(7, D2, NEO_GRB + NEO_KHZ800);
 
 bool on = true;
 
 void handleRoot()
 {
-  digitalWrite(led, 1);
-  server.send(200, "text/plain", "TEST");
-  digitalWrite(led, 0);
-}
-
-void handleNotFound()
-{
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++)
-  {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
+  server.send(200, "text/html", "\n\
+<html>\n\
+\n\
+<head>\n\
+    <title>Der Nico Alarm</title>\n\
+\n\
+    <style>\n\
+        body,\n\
+        html {\n\
+            width: 100%;\n\
+            height: 100%;\n\
+        }\n\
+\n\
+        body {\n\
+            margin: 0;\n\
+            background: #ffffff;\n\
+        }\n\
+\n\
+        h1 {\n\
+            text-align: center;\n\
+            margin: 0.5em 0;\n\
+        }\n\
+\n\
+        .options {\n\
+            overflow: auto;\n\
+            display: flex;\n\
+            row-gap: 1em;\n\
+            column-gap: 1em;\n\
+            flex-wrap: wrap;\n\
+            padding: 1em;\n\
+        }\n\
+\n\
+        button {\n\
+            font-size: 2em;\n\
+            border-radius: 4px;\n\
+            padding: 0.5em;\n\
+            background: #6060ff;\n\
+            color: #ffffff;\n\
+        }\n\
+    </style>\n\
+\n\
+    <script>\n\
+        function request(what) {\n\
+            const xhr = new XMLHttpRequest()\n\
+\n\
+            xhr.open('GET', '/' + what)\n\
+\n\
+            xhr.send()\n\
+        }\n\
+\n\
+        function on() {\n\
+            return request('on')\n\
+        }\n\
+\n\
+        function off() {\n\
+            return request('off')\n\
+        }\n\
+    </script>\n\
+</head>\n\
+\n\
+<body>\n\
+    <h1>Der Nico Alarm</h1>\n\
+    <div class='options'>\n\
+        <button onclick='on()'>Anschalten</button>\n\
+        <button onclick='off()'>Auschalten</button>\n\
+    </div>\n\
+</body>\n\
+\n\
+</html>\n\
+");
 }
 
 void setup(void)
 {
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
+  pinMode(D2, OUTPUT);
+
   Serial.begin(115200);
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  Serial.println("");
 
-  // Wait for connection
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
-    Serial.print(".");
   }
-  Serial.println("");
+
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("esp8266"))
-  {
-    Serial.println("MDNS responder started");
-  }
+  MDNS.begin("esp8266");
 
   server.on("/", handleRoot);
 
@@ -84,30 +128,7 @@ void setup(void)
         server.send(200, "text/plain", "OFF");
       });
 
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
-
-  server.on("/gif", []() {
-    static const uint8_t gif[] PROGMEM = {
-        0x47, 0x49, 0x46, 0x38, 0x37, 0x61, 0x10, 0x00, 0x10, 0x00, 0x80, 0x01,
-        0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0x2c, 0x00, 0x00, 0x00, 0x00,
-        0x10, 0x00, 0x10, 0x00, 0x00, 0x02, 0x19, 0x8c, 0x8f, 0xa9, 0xcb, 0x9d,
-        0x00, 0x5f, 0x74, 0xb4, 0x56, 0xb0, 0xb0, 0xd2, 0xf2, 0x35, 0x1e, 0x4c,
-        0x0c, 0x24, 0x5a, 0xe6, 0x89, 0xa6, 0x4d, 0x01, 0x00, 0x3b};
-    char gif_colored[sizeof(gif)];
-    memcpy_P(gif_colored, gif, sizeof(gif));
-    // Set the background to a random set of colors
-    gif_colored[16] = millis() % 256;
-    gif_colored[17] = millis() % 256;
-    gif_colored[18] = millis() % 256;
-    server.send(200, "image/gif", gif_colored, sizeof(gif_colored));
-  });
-
-  server.onNotFound(handleNotFound);
-
   server.begin();
-  Serial.println("HTTP server started");
 
   pixels.begin();
 }
@@ -136,12 +157,13 @@ void green()
 
   at = (at + 1) % 21;
 
-  delay(100);
+  delay(250);
 }
 
 void loop(void)
 {
   server.handleClient();
+
   MDNS.update();
 
   green();
